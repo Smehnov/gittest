a546838f10d005baf7a7512e5c8836c7c458bcf9
Anastasia <paw.nastia2016@yandex.ru>
2023-09-13 20:44:06 +0300

    r12

--- B.java
+++ B.java
@@ -23,4 +23,8 @@ public class B extends J {
     public java.util.Set<Integer> ll() {
         return new java.util.LinkedList<Integer>;
     }
+
+    public byte oo() {
+        return 3;
+    }
 }
